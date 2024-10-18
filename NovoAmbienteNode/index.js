d1801022b82dd656f9ab6d0bc336552a60e4099e
Matheus-Marques-de-oliveira <130381677+Matheus-Marques-de-oliveira@users.noreply.github.com>
2024-10-18 20:15:44 -0300

    Ambiente Node
    codigo com node.js implementado

---
+++ NovoAmbienteNode/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+console.dir(http);
+/*
+const arr = [1,5,6,8,12,32,3]
+console.table (arr)
+console.dir (arr)
+console.error ("Error!")
+console.warn ("Attemption")
+console.info (arr)
+console.time ()
+
+console.log (arr)
+
+console.time ()
+*/
